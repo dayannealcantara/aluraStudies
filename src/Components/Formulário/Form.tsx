9636984b05eddd3cc4faf0c9d0b7ef512bfdde51
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-06-24 16:05:32 -0300

    feat:creating form

--- src/Components/Formulário/Form.tsx
+++ src/Components/Formulário/Form.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
 import Button from '../Button/Button';
 import './FormStyle.scss';
+import React, { useState } from 'react';
+import { ITarefa } from '../../types/tarefa';
+import { v4 as uuidv4 } from 'uuid';
 
-function Form() {
+type FormProps = {
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+};
+
+function Form({ setTarefas }: FormProps) {
+  const [tarefa, setTarefa] = useState('');
+  const [tempo, setTempo] = useState('00:00');
+
+  function adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      {
+        tarefa,
+        tempo,
+        selecionado: false,
+        completado: false,
+        id: uuidv4(),
+      },
+    ]);
+    setTarefa('');
+    setTempo('00:00');
+  }
+
   return (
-    <form className="novaTarefa">
+    <form className="novaTarefa" onSubmit={adicionarTarefa}>
       <div className="inputContainer">
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input
           type="text"
           id="tarefa"
           name="tarefa"
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)}
           placeholder="O que você quer estudar?"
           required
         />
@@ -19,16 +46,17 @@ function Form() {
         <label htmlFor="tempo">Tempo</label>
         <input
           type="time"
-          id="time"
+          id="tempo"
           name="tempo"
+          value={tempo}
+          onChange={(e) => setTempo(e.target.value)}
           step="1"
           min="00:00:00"
           max="01:30:00"
           required
         />
       </div>
-
-      <Button />
+      <Button type="submit"> Adicionar</Button>
     </form>
   );
 }
