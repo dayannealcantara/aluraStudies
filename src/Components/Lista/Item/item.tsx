086bd9ec422e05324dc5ea1e451c2262d9546419
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-06-24 16:04:40 -0300

    feat:creating task record location

---
+++ src/Components/Lista/Item/item.tsx
@@ -0,0 +1,34 @@
+import { ITarefa } from '../../../types/tarefa';
+
+interface Props extends ITarefa {
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
+}
+
+function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  return (
+    <li
+      onClick={() =>
+        !completado &&
+        selecionaTarefa({
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id,
+        })
+      }
+    >
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  );
+}
+
+export default Item;
