057d836af7a9eac965167532065d2ae69c3b5ab6
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-06-24 16:03:54 -0300

    feat: creating and styling watch

---
+++ src/Components/Cronometro/Relogio/Relogio.tsx
@@ -0,0 +1,23 @@
+import './RelogioStyle.scss';
+
+type RelogioProps = {
+  tempo: number | undefined;
+};
+
+function Relogio({ tempo = 0 }: RelogioProps) {
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+  const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0');
+  const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
+
+  return (
+    <>
+      <span className="relogioNumero">{minutoDezena}</span>
+      <span className="relogioNumero">{minutoUnidade}</span>
+      <span className="relogioDivisao">:</span>
+      <span className="relogioNumero">{segundoDezena}</span>
+      <span className="relogioNumero">{segundoUnidade}</span>
+    </>
+  );
+}
+export default Relogio;
