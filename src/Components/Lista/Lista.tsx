c4eaafd95ed0179e1bac03bbb310233685e2835a
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-06-24 16:05:07 -0300

    feat: finalizing component and list style

--- src/Components/Lista/Lista.tsx
+++ src/Components/Lista/Lista.tsx
@@ -1,29 +1,18 @@
-import React from 'react';
 import './ListaStyle.scss';
-function Lista() {
-  const tarefas = [
-    {
-      tarefa: 'React',
-      tempo: '02:00:00',
-    },
-    {
-      tarefa: 'JavaScript',
-      tempo: '01:00:00',
-    },
-    {
-      tarefa: 'TypeScript',
-      tempo: '01:30:00',
-    },
-  ];
+import { ITarefa } from '../../types/tarefa';
+import Item from '../Lista/Item/item';
+
+type ListaProps = {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void,
+};
+function Lista({ tarefas, selecionaTarefa }: ListaProps) {
   return (
     <aside className="listaTarefas">
       <h2>Estudos do dia</h2>
       <ul>
-        {tarefas.map((tarefa, index) => (
-          <li key={index} className="item">
-            <h3>{tarefa.tarefa}</h3>
-            <span>{tarefa.tempo}</span>
-          </li>
+        {tarefas.map((item) => (
+          <Item selecionaTarefa={selecionaTarefa} key={item.id} {...item} />
         ))}
       </ul>
     </aside>
